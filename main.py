5a234e023ee1542d66122cc1e404b2010eaf244e
Ritesh Sharma <riteshsharma.bit@gmail.com>
2024-08-11 21:58:23 +0530

    1-actor-agent

---
+++ main.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain_core.output_parsers.openai_tools import (
+    JsonOutputToolsParser,
+    PydanticToolsParser,
+)
+from langchain_core.messages import HumanMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_openai import ChatOpenAI
+
+if __name__ == "__main__":
+    print("Hello LangGraph Reflexion agent sample")
---
+++ schemas.py
@@ -0,0 +1,16 @@
+from typing import List
+from langchain_core.pydantic_v1 import BaseModel, Field
+
+class Reflection(BaseModel):
+    missing: str = Field(description="Critique of what is missing.")
+    superfluous: str = Field(description="Critique of what is superfluous")
+    # superfluous means unnecessary information
+
+class AnswerQuestion(BaseModel):
+    """ Answer the question. """
+
+    answer: str = Field(description="~250 words detailed answer to the question.")
+    reflection: Reflection = Field(description="Your reflection to the initial answer.")
+    search_queries: List[str] = Field(
+        description="1-3 search queries for researching improvements to address the critique of your current answer."
+    )
